d6a47af9b3334a20559d91ad7ed4f51f41dfdefc
Brendon Sanyahawa <92976518+taylorcool7@users.noreply.github.com>
2023-06-28 20:18:41 -0300

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoadingComponent } from './loading/loading.component';
+import { ServiceComponent } from './service/service.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoadingComponent,
+    ServiceComponent
   ],
   imports: [
     BrowserModule,
